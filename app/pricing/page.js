b6c9a8115ad9e9fae43b86ded04cdd6f7f4b6303
Adel <owikawa01@gmail.com>
2026-09-12 20:20:23 +0300

    Update pricing page to use Paddle.js

--- app/pricing/page.js
+++ app/pricing/page.js
@@ -1,7 +1,7 @@
 "use client";
 
-import { useState } from 'react';
-import { supabase } from '../../lib/supabaseClient';
+import { useState, useEffect } from 'react';
+import Script from 'next/script';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 
